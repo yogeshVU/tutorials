ec59d9f9a977a0ca70c02960da9152c362231334
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2020-02-20 16:10:28 -0600

    update

--- java-concurrency/src/com/goalabs/threading/MySynchronize.java
+++ java-concurrency/src/com/goalabs/threading/MySynchronize.java
@@ -1,4 +1,62 @@
 package com.goalabs.threading;
 
+class ThreadedPrinter extends Thread{
+    @Override
+    public synchronized void run() {
+        for(int i=0;i<10;i++){
+            System.out.println("Printing value from Thread: "+i+": "+Thread.currentThread().getName());
+        }
+    }
+}
+
+class RunnablePrinter implements Runnable{
+    @Override
+    public synchronized void run() {
+        for(int i=0;i<10;i++){
+            System.out.println("Printing value: "+i+": "+Thread.currentThread().getName());
+        }
+    }
+}
+
+class MyDocument{
+    private ThreadedPrinter ptr;
+    private Runnable rptr;
+
+    public MyDocument(ThreadedPrinter tempPtr){
+        this.ptr = tempPtr;
+    }
+
+    public MyDocument(RunnablePrinter tempPtr){
+        this.rptr = tempPtr;
+    }
+
+    public void submit(){
+        if(this.ptr!=null)
+        this.ptr.start();
+
+        if(this.rptr!=null)
+            this.rptr.run();
+    }
+
+}
+
+
+
 public class MySynchronize {
+
+    public static void main(String[] args){
+        MyDocument d1 = new MyDocument(new ThreadedPrinter());
+        MyDocument d2 = new MyDocument(new ThreadedPrinter());
+
+//        d1.submit();
+//        d2.submit();
+
+        MyDocument d3 = new MyDocument(new RunnablePrinter());
+        MyDocument d4 = new MyDocument(new RunnablePrinter());
+
+        d3.submit();
+        d4.submit();
+
+    }
+
 }
---
+++ java-concurrency/src/com/goalabs/threadpool/MyFixedThreadpool.java
@@ -0,0 +1,4 @@
+package com.goalabs.threadpool;
+
+public class MyFixedThreadpool {
+}
