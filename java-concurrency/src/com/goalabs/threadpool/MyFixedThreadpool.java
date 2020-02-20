29771d2d913c099496a09b31f93a21e89bb83929
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2020-02-20 16:39:40 -0600

    update

--- java-concurrency/src/com/goalabs/threadpool/MyFixedThreadpool.java
+++ java-concurrency/src/com/goalabs/threadpool/MyFixedThreadpool.java
@@ -1,4 +1,35 @@
 package com.goalabs.threadpool;
 
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class MyTask implements Runnable{
+
+    private volatile int counter = 0;
+    @Override
+    public synchronized void run() {
+
+        for(int i =0;i<100;i++){
+            counter++;
+        }
+        System.out.println("counter "+counter);
+
+    }
+}
+
 public class MyFixedThreadpool {
+
+    public static void main(String[] args){
+        int numberofcores = Runtime.getRuntime().availableProcessors();
+        System.out.println("Number of Processors: "+ numberofcores);
+        ExecutorService myservice = Executors.newFixedThreadPool(numberofcores);
+        for(int i =0; i<1000;i++)
+            myservice.execute(new MyTask());
+
+        myservice.shutdown();
+
+    }
+
 }
