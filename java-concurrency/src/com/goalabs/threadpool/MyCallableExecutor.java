0e89ed5431606edf16e4765a09eb2d03242f1aa0
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2020-02-21 13:42:13 -0600

    update

---
+++ java-concurrency/src/com/goalabs/threadpool/MyCallableExecutor.java
@@ -0,0 +1,57 @@
+package com.goalabs.threadpool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimerTask;
+import java.util.concurrent.*;
+import java.util.stream.StreamSupport;
+
+class MyCallableTask implements Callable{
+    @Override
+    public Object call() throws Exception {
+
+//        Get the time of computation
+        Long currentTime = System.currentTimeMillis();
+        TimeUnit.MILLISECONDS.sleep(100);
+
+        return  (System.currentTimeMillis()-currentTime);
+
+    }
+}
+
+public class MyCallableExecutor {
+
+    public ExecutorService MyService;
+
+    public MyCallableExecutor(){
+        this.MyService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        MyCallableExecutor tmp = new MyCallableExecutor();
+        List<Future<Long>> myFuture  = new ArrayList<Future<Long>>();
+
+        for(int i =0;i<100;i++){
+            Future future = tmp.MyService.submit(new MyCallableTask());
+            myFuture.add(future);
+        }
+
+        for (Future<Long> result : myFuture)
+        {
+            Long number = null;
+            try {
+                number = result.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("Main: Task %d\n", number);
+        }
+        tmp.MyService.shutdown();
+        tmp.MyService.awaitTermination(1000,TimeUnit.MILLISECONDS);
+
+    }
+
+}
--- java-concurrency/src/com/goalabs/threadpool/MyFixedThreadpool.java
+++ java-concurrency/src/com/goalabs/threadpool/MyFixedThreadpool.java
@@ -4,31 +4,49 @@ package com.goalabs.threadpool;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 class MyTask implements Runnable{
 
-    private volatile int counter = 0;
+    public static volatile int counter = 0;
     @Override
     public synchronized void run() {
 
-        for(int i =0;i<100;i++){
+//        for(int i =0;i<100;i++){
             counter++;
+//        }
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        System.out.println("counter "+counter);
+//        System.out.println("counter "+counter);
 
     }
 }
 
 public class MyFixedThreadpool {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         int numberofcores = Runtime.getRuntime().availableProcessors();
         System.out.println("Number of Processors: "+ numberofcores);
+        long startProcessingTime = System.currentTimeMillis();
+
         ExecutorService myservice = Executors.newFixedThreadPool(numberofcores);
+
         for(int i =0; i<1000;i++)
             myservice.execute(new MyTask());
 
+        long endProcessingTime = System.currentTimeMillis();
+
+        long totaltime = endProcessingTime - startProcessingTime;
+
+
         myservice.shutdown();
+        myservice.awaitTermination(300, TimeUnit.SECONDS);
+        System.out.println("Total Completion time is: "+totaltime);
+        System.out.println("Counter Value: "+MyTask.counter);
+
 
     }
 
