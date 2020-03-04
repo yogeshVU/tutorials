5447752b37c4d5c9636b4e9060471007964320e9
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2020-03-04 14:25:32 -0600

    update

--- java-collections/src/main/java/com/goalabs/map/MyCopyMap.java
+++ java-collections/src/main/java/com/goalabs/map/MyCopyMap.java
@@ -10,51 +10,49 @@ import java.util.Map;
 import edu.emory.mathcs.backport.java.util.TreeMap.Entry;
 
 public class MyCopyMap {
-    public static <Integer,String> LinkedHashMap<Integer,String> myCopy1(LinkedHashMap<Integer,String> tmpMap) {
+    public static <Integer, String> LinkedHashMap<Integer, String> myCopy1(LinkedHashMap<Integer, String> tmpMap) {
 
         // tmpMap.forEach( (k,v) -> System.out.println(k.toString() + "--" +v));
-        return (LinkedHashMap<Integer,String>) tmpMap.clone();
+        return (LinkedHashMap<Integer, String>) tmpMap.clone();
     }
 
-
-    public static <Integer, String> LinkedHashMap<Integer, String> copyUsingPutAll(LinkedHashMap<Integer,String> tmpMap) {
-        LinkedHashMap<Integer,String> shallowCopy = new LinkedHashMap<Integer,String>();
+    public static <Integer, String> LinkedHashMap<Integer, String> copyUsingPutAll(
+            LinkedHashMap<Integer, String> tmpMap) {
+        LinkedHashMap<Integer, String> shallowCopy = new LinkedHashMap<Integer, String>();
         shallowCopy.putAll(tmpMap);
         return shallowCopy;
     }
-    
 
-    public static <Integer,String> LinkedHashMap<Integer,String> copyUsingPut(LinkedHashMap<Integer,String> tmpMap){
-        Set<Map.Entry<Integer,String>> myset = tmpMap.entrySet();
-        LinkedHashMap<Integer,String> newTmpMap = new LinkedHashMap<Integer,String>();
+    public static <Integer, String> LinkedHashMap<Integer, String> copyUsingPut(LinkedHashMap<Integer, String> tmpMap) {
+        Set<Map.Entry<Integer, String>> myset = tmpMap.entrySet();
+        LinkedHashMap<Integer, String> newTmpMap = new LinkedHashMap<Integer, String>();
 
-        for (Map.Entry<Integer,String> tmpEntry: myset){
+        for (Map.Entry<Integer, String> tmpEntry : myset) {
             newTmpMap.put(tmpEntry.getKey(), tmpEntry.getValue());
         }
         return newTmpMap;
     }
 
-    public static void traverseMap(HashMap<Integer,String> tmpMap){
+    public static void traverseMap(HashMap<Integer, String> tmpMap) {
 
-        tmpMap.forEach((k,v) -> System.out.println(k.toString() + "~~" + v ));
+        tmpMap.forEach((k, v) -> System.out.println(k.toString() + "~~" + v));
     }
 
-
     public static void main(String[] args) {
-        
-        LinkedHashMap<Integer,String> myHashMap = new LinkedHashMap<Integer,String>();
-        for(Integer i=0; i<10;i ++){
-            myHashMap.put(i, "Hello_"+i.toString());
+
+        LinkedHashMap<Integer, String> myHashMap = new LinkedHashMap<Integer, String>();
+        for (Integer i = 0; i < 10; i++) {
+            myHashMap.put(i, "Hello_" + i.toString());
         }
-        LinkedHashMap<Integer,String> tmp2 = MyCopyMap.myCopy1(myHashMap);
+        LinkedHashMap<Integer, String> tmp2 = MyCopyMap.myCopy1(myHashMap);
         myHashMap.put(12, "HelloDoctor");
         // MyCopyMap.traverseMap(tmp2);
 
-        LinkedHashMap<Integer,String> tmp3 = MyCopyMap.copyUsingPutAll(myHashMap);
+        LinkedHashMap<Integer, String> tmp3 = MyCopyMap.copyUsingPutAll(myHashMap);
         MyCopyMap.traverseMap(tmp3);
 
-        tmp3.put(13, "value");    
-        LinkedHashMap<Integer,String> tmp4 = MyCopyMap.copyUsingPut(tmp3);
+        tmp3.put(13, "value");
+        LinkedHashMap<Integer, String> tmp4 = MyCopyMap.copyUsingPut(tmp3);
         MyCopyMap.traverseMap(tmp4);
 
     }
